a76a231726e6bfdbc9037a5610a9b2b2b8a637da
Luckyones <mr.heromak@gmail.com>
2022-03-15 02:13:43 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(57, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(701, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Lowest",
+            "BelowNormal",
+            "Normal",
+            "AboveNormal",
+            "Highest"});
+            this.comboBox1.Location = new System.Drawing.Point(439, 308);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Normal";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Lowest",
+            "BelowNormal",
+            "Normal",
+            "AboveNormal",
+            "Highest"});
+            this.comboBox2.Location = new System.Drawing.Point(439, 341);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "Normal";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Lowest",
+            "BelowNormal",
+            "Normal",
+            "AboveNormal",
+            "Highest"});
+            this.comboBox3.Location = new System.Drawing.Point(439, 369);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.Text = "Normal";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "10",
+            "20",
+            "30",
+            "40",
+            "50",
+            "60",
+            "70",
+            "80",
+            "90",
+            "100"});
+            this.comboBox4.Location = new System.Drawing.Point(634, 308);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 6;
+            this.comboBox4.Text = "Buffer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+
+namespace WindowsFormsApp5
+{
+
+    public partial class Form1 : Form
+    {
+
+        //делегат для связи с основным потоком
+        private delegate void DisplayGraphDelegate(int x);
+        public Queue Qe = new Queue();
+        static Semaphore s1 = new Semaphore(1, 2);
+        static Semaphore s2 = new Semaphore(1, 1);
+        Thread p1;
+        Thread p2;
+        Thread p3;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void addComboBoxItems()
+        {
+            string[] items =
+            {
+                ThreadPriority.Lowest.ToString(),
+                ThreadPriority.BelowNormal.ToString(),
+                ThreadPriority.Normal.ToString(),
+                ThreadPriority.AboveNormal.ToString(),
+                ThreadPriority.Highest.ToString()
+            };
+
+            comboBox1.Items.AddRange(items);
+            comboBox2.Items.AddRange(items);
+            comboBox3.Items.AddRange(items);
+        }
+
+        private void thread1()
+        {
+            var thread = new Thread(Producer);
+            thread.IsBackground = true;
+            foreach (ThreadPriority value in Enum.GetValues(typeof(ThreadPriority)))
+            {
+                if (value.ToString() == comboBox1.Text)
+                    thread.Priority = value;
+            }
+            thread.Start();
+        }
+
+        private void thread2()
+        {
+            var thread = new Thread(Producer);
+            thread.IsBackground = true;
+            foreach (ThreadPriority value in Enum.GetValues(typeof(ThreadPriority)))
+            {
+                if (value.ToString() == comboBox2.Text)
+                    thread.Priority = value;
+            }
+            thread.Start();
+        }
+        private void thread3()
+        {
+            var thread = new Thread(Consumer);
+            thread.IsBackground = true;
+            foreach (ThreadPriority value in Enum.GetValues(typeof(ThreadPriority)))
+            {
+                if (value.ToString() == comboBox3.Text)
+                    thread.Priority = value;
+            }
+            thread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1 = new Thread(Producer);
+            p2 = new Thread(Producer);
+            p3 = new Thread(Consumer);
+            p1.Start(0);
+            p2.Start(1);
+            p3.Start();
+        }
+
+
+        private void AddEntries(int x)
+        {
+            listBox1.Items.Add(x);
+        }
+
+        private void Producer(object i) // Производитель
+        {
+            while (true)
+            {
+                s1.WaitOne();
+                Monitor.Enter(Qe);
+                if (Qe.Count < 20)
+                    Qe.Enqueue(i);
+                else
+                    listBox1.Invoke(new DisplayGraphDelegate(AddEntries), Qe.Dequeue());
+                Thread.Sleep(1);
+                Monitor.Exit(Qe);
+                s1.Release();
+            }
+        }
+
+        public void Consumer() // Потребитель
+        {
+            string y = "get";
+            while (true)
+            {
+                s2.WaitOne();
+                if (Qe.Count < 1)
+                {
+                    Thread.Sleep(100);
+                }
+                else
+
+                    listBox1.Invoke(new Action(() => listBox1.Items.Add(y)));
+                Thread.Sleep(100);
+                s2.Release();
+            }
+        }
+
+        [Serializable]
+        public class Customer
+        {
+            private string nameValue = string.Empty;
+            public Customer(String name)
+            {
+                nameValue = name;
+            }
+            public string Name
+            {
+                get { return nameValue; }
+                set { nameValue = value; }
+            }
+
+            public void Buff(int value)
+            {
+                int BUFF = value; // буфер
+                byte[] but = new byte[BUFF]; // кладем буфер
+
+            }
+
+
+
+
+        }
+    }
+}
+
+   
+            
+                
+                   
+            
+         
+
+
+
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading;
+
+namespace ExempleSynsCostumerPoducer
+{
+class MainClass
+{
+
+
+public MainClass()
+{
+p1 = new Thread(Producer);
+p2 = new Thread(Producer);
+p3 = new Thread(Consumer);
+p1.Start(0);
+p2.Start(1);
+p3.Start();
+}
+
+private void Producer(object i) // Производитель
+{
+while (true)
+{
+s1.WaitOne();
+Monitor.Enter(Qe);
+if (Qe.Count < 20)
+Qe.Enqueue(i);
+else
+Thread.Sleep(1);
+Monitor.Exit(Qe);
+s1.Release();
+}
+}
+public void Consumer() // Потребитель
+{
+while (true)
+{
+s2.WaitOne();
+if (Qe.Count < 1)
+{
+Thread.Sleep(1);
+}
+else
+Console.Write(Qe.Dequeue());
+s2.Release();
+}
+}
+
+static void Main()
+{
+new MainClass();
+}
+}
+}
+ */
